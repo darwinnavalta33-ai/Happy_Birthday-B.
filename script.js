72e2bb87f49871eaa11c7e039e47fdfd438f38b6
darwinnavalta33-ai <darwinnavalta33@gmail.com>
2025-08-31 14:55:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `Hi BRINET!!\n\nI wish you could see yourself through my eyes — you’d finally understand how amazing you truly are.✨ \n\nKeep shining, the world needs your light. \n\nLike I could ever forget… happy birthday, Gorgeous💕.\n\nHope today spoils you as much as you deserve. `;
+const message = `Hi BRINET!!\n\nTo the one who makes my heart race a little faster. I hope your day is as amazing as you are✨. \n\nKeep shining, the world needs your light. \n\nLike I could ever forget… happy birthday, Gorgeous💕.\n\nHope today spoils you as much as you deserve. `;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
@@ -14,7 +14,7 @@ function showLetter() {
       if (i < message.length) {
         typedText.innerHTML += message.charAt(i);
         i++;
-        setTimeout(typeWriter, 30);
+        setTimeout(typeWriter, 10);
       }
     }
 
