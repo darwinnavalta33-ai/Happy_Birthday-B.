ee2955d37cc65d58a5d5699c956b2c7c4bb741d8
darwinnavalta33-ai <darwinnavalta33@gmail.com>
2025-08-31 10:30:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const message = `Just a reminder\n\nYou’re stronger, brighter, and more amazing than you give yourself credit for.\n\nKeep shining, the world needs your light. \n\nI wish you could see yourself through my eyes — you’d finally understand how amazing you truly are.✨ \n\nLike I could ever forget… happy birthday, Gorgeous💕.\n\nHope today spoils you as much as you deserve.`;
+
+function showLetter() {
+  document.getElementById("introText").style.opacity = 0;
+  document.querySelector(".btn").style.display = "none";
+
+  setTimeout(() => {
+    const letterBox = document.getElementById("letterBox");
+    const typedText = document.getElementById("typedText");
+    letterBox.style.display = "block";
+    let i = 0;
+
+    function typeWriter() {
+      if (i < message.length) {
+        typedText.innerHTML += message.charAt(i);
+        i++;
+        setTimeout(typeWriter, 30);
+      }
+    }
+
+    typeWriter();
+  }, 600);
+}
